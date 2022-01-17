eb9d33aa372ecae2bf121f317f51ca14bfe163af
verson205 <97358522+verson205@users.noreply.github.com>
2022-01-17 19:39:52 +0200

    Add files via upload

--- driver/design/thumbnail.py
+++ driver/design/thumbnail.py
@@ -17,34 +17,34 @@ async def thumb(thumbnail, title, userid, ctitle):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg", mode="wb")
+                f = await aiofiles.open(f"search/thumb{userid}.png", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
-    image1 = Image.open(f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg")
+    image1 = Image.open(f"search/thumb{userid}.png")
     image2 = Image.open("driver/source/LightBlue.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
-    Image.alpha_composite(image5, image6).save(f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg")
-    img = Image.open(f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg")
+    Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
+    img = Image.open(f"search/temp{userid}.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("driver/source/regular.ttf", 52)
     font2 = ImageFont.truetype("driver/source/medium.ttf", 76)
     draw.text(
         (25, 610),
         f"{title[:18]}...",
-        fill="red",
+        fill="black",
         font=font2,
     )
     draw.text(
         (27, 535),
-        f"play now {ctitle[:8]}...",
-        fill="red",
+        f"Playing on {ctitle[:8]}...",
+        fill="black",
         font=font,
     )
-    img.save(f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg")
-    os.remove(f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg")
-    os.remove(f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg")
-    final = f"https://telegra.ph/file/a2b3e05ff5b0f32fe389b.jpg"
+    img.save(f"search/final{userid}.png")
+    os.remove(f"search/temp{userid}.png")
+    os.remove(f"search/thumb{userid}.png")
+    final = f"search/final{userid}.png"
     return final
